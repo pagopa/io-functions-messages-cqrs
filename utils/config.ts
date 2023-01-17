4868128b7c6488e5b5ffd0b9e243d25eadb58f43
AleDore <alessio85.dore@gmail.com>
2023-01-17 11:45:49 +0100

    refactor withDefault

--- utils/config.ts
+++ utils/config.ts
@@ -8,16 +8,23 @@
 import * as t from "io-ts";
 
 import * as E from "fp-ts/Either";
+import * as O from "fp-ts/Option";
 import { pipe } from "fp-ts/lib/function";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 
 import { AzureEventhubSasFromString } from "@pagopa/fp-ts-kafkajs/dist/lib/KafkaProducerCompact";
 import { IsoDateFromString } from "@pagopa/ts-commons/lib/dates";
+import { withDefault } from "@pagopa/ts-commons/lib/types";
 
+// Default start from beginning
+export const DEFAULT_MESSAGE_CHANGE_FEED_START_TIME = "2010-01-01T01:00:00Z";
 export const MessageChangeFeedConfig = t.type({
   MESSAGE_CHANGE_FEED_LEASE_PREFIX: NonEmptyString,
-  MESSAGE_CHANGE_FEED_START_TIME: NonEmptyString
+  MESSAGE_CHANGE_FEED_START_TIME: withDefault(
+    NonEmptyString,
+    DEFAULT_MESSAGE_CHANGE_FEED_START_TIME as NonEmptyString
+  )
 });
 export type MessageChangeFeedConfig = t.TypeOf<typeof MessageChangeFeedConfig>;
 // global app configuration
@@ -58,15 +65,14 @@ export const IConfig = t.intersection([
   MessageChangeFeedConfig
 ]);
 
-// Default start from beginning
-const DEFAULT_MESSAGE_CHANGE_FEED_START_TIME = "2010-01-01T01:00:00Z";
 export const envConfig = {
   ...process.env,
   MESSAGE_CHANGE_FEED_START_TIME: pipe(
     process.env.MESSAGE_CHANGE_FEED_START_TIME,
     IsoDateFromString.decode,
-    E.map(dateFromString => dateFromString.toISOString()),
-    E.getOrElse(() => DEFAULT_MESSAGE_CHANGE_FEED_START_TIME)
+    O.fromEither,
+    O.map(dateFromString => dateFromString.toISOString()),
+    O.toUndefined
   ),
   isProduction: process.env.NODE_ENV === "production"
 };
