5287fe44cb471367b7008245a16adf8763e027fd
garma00 <giovanniarmaleo00@gmail.com>
2023-02-08 09:25:23 +0100

    fix build

--- clients/payment-updater.ts
+++ clients/payment-updater.ts
@@ -1,9 +1,5 @@
 import { agent } from "@pagopa/ts-commons";
-import {
-  AbortableFetch,
-  setFetchTimeout,
-  toFetch
-} from "@pagopa/ts-commons/lib/fetch";
+import { AbortableFetch, setFetchTimeout } from "@pagopa/ts-commons/lib/fetch";
 import { Millisecond } from "@pagopa/ts-commons/lib/units";
 import nodeFetch from "node-fetch";
 import { createClient } from "../generated/payment-updater/client";
@@ -18,9 +14,17 @@ export const apimSubscriptionKey = config.APIM_SUBSCRIPTION_KEY;
 // 5 seconds timeout by default
 const DEFAULT_REQUEST_TIMEOUT_MS = 10000;
 
+// this method can be removed after we migrate @pagopa/ts-commons to node 18 so we can use the new
+// implementation of  toFetch
+// eslint-disable-next-line
+const toRemoveToFetch = (f: any) => (
+  input: RequestInfo | URL,
+  init?: RequestInit
+) => f(input, init).e1;
+
 // Must be an https endpoint so we use an https agent
 const abortableFetch = AbortableFetch(agent.getHttpsFetch(process.env));
-const fetchWithTimeout = toFetch(
+const fetchWithTimeout = toRemoveToFetch(
   setFetchTimeout(DEFAULT_REQUEST_TIMEOUT_MS as Millisecond, abortableFetch)
 );
 const fetchApi: typeof fetchWithTimeout = (nodeFetch as unknown) as typeof fetchWithTimeout;
